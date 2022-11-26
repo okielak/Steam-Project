7cf93981fee6ac83fdd5733290e3882e90651ce7
Oliwia <oliwiakielak99@gmail.com>
2022-11-26 09:26:54 +0100

    commit push

---
+++ main.py
@@ -0,0 +1,9 @@
+
+def get_user_name():
+    #symulujemy pobranie z bazy danych
+    first_name = "Oliwia"
+    second_name = "Kielak"
+    return f (first_name) (second_name)
+
+
+print(f"Jesteś zalogowanyy jako (get_user_name())")
